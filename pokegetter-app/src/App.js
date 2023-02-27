e03d1e2fae7f65b60dc6c6a2307ca6ba33a5f248
phil-p-s <pstanton@mines.edu>
2023-02-27 13:51:58 -0700

    Update App.js

--- pokegetter-app/src/App.js
+++ pokegetter-app/src/App.js
@@ -1,13 +1,12 @@
 import { useState } from "react";
-
 function App() {
-    const [quote, setQuote] = useState(null);
-    return ( <
-            div > {
-                quote ? ( < h1 > The quote would be rendered here < /h1>): null} <
-                    /div>
-                )
+  const [quote, setQuote] = useState(null);
+  return(
+    <div>
+    {quote ? (<h1>The quote would be rendered here</h1>): null}
+    </div>
+  )
 
-            }
+  }
 
-            export default App;
+export default App;
